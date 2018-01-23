2d35c1b0a845474c5c0823ee6574b189fecd074a
刘佳杰 <bob.liu@liujiajiedeMacBook-Pro.local>
2018-01-23 15:38:19 +0800

    完成这次的联系

--- express-mongoose/routes/users.js
+++ express-mongoose/routes/users.js
@@ -6,6 +6,11 @@ var userModel = require('../models/userModel.js');
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/detail/:_id', function(req, res, next) {
+  userModel.findOne({_id: req.params._id}, function(err, data) {
+    res.render('UserDetail', data);
+  })
+})
 router.get('/list', function(req, res, next) {
   userModel.find(function(err, users) {
     if (err) {
@@ -21,7 +26,33 @@ router.get('/add', function(req, res, next) {
   res.render('UserAdd');
 });
 router.post('/add', function(req, res, next) {
-  console.log(req.body);
+  var newUser = new userModel(req.body);
+
+  newUser.save(function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect('/users/list');
+  })
 });
+router.get('/edit/:_id', function(req, res, next) {
+  userModel.findOne(req.params, function(err, data) {
+    res.render('UserEdit', data);
+  })
+});
+router.post('/edit/:_id', function(req, res, next) {
+  userModel.update({_id:req.params._id}, {$set: req.body}, function(err, data) {
+    res.redirect('/users/detail/' + req.params._id);
+  })
+});
+router.delete('/del', function(req, res) {
+  userModel.remove({_id: req.query.id}, function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    res.json({code:200, msg:'删除成功'});
+  })
+})
 
 module.exports = router;
