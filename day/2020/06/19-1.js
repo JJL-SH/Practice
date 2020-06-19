3f827b8b393be24283f6982edf854fdb7a24de91
刘佳杰 <liuchinash@outlook.com>
2020-06-19 14:57:16 +0800

    create dev

---
+++ day/2020/06/19-1.js
@@ -0,0 +1,44 @@
+var throttle = function (fn, interval = 100) {
+  var _target = fn;
+  var _timer;
+  var _firstTime = true;
+
+  return function () {
+    var _args = arguments;
+    var _self = this;
+
+    if (_firstTime) {
+      _target.apply(_self, _args);
+      return (_firstTime = false);
+    }
+
+    if (_timer) {
+      return false;
+    }
+
+    _timer = setTimeout(function () {
+      clearTimeout(_timer);
+      timer = null;
+      _target.apply(_self, _args);
+    }, interval);
+  };
+};
+
+var CreateDiv = (function () {
+  var instance;
+  var CreateDiv = function (html) {
+    if (instance) {
+      return instance;
+    }
+    this.html = html;
+    this.init();
+    return (instance = this);
+  };
+  CreateDiv.prototype.init = function () {
+    var div = document.createElement("div");
+    div.innerHTML = this.html;
+    document.body.appendChild(div);
+  };
+
+  return CreateDiv;
+})();
